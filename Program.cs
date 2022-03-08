c6aa16100a410efe3c529e2cb001ff12e533f929
rido-min <rido-min@users.noreply.github.com>
2022-03-07 17:39:09 -0800

    more units

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 using Microsoft.Azure.DigitalTwins.Parser;
 using Microsoft.Azure.DigitalTwins.Parser.Models;
 
-string readFileFromDtmi()
+string ReadFile()
 {
     var basePath = System.Reflection.Assembly.GetExecutingAssembly().Location + @"./../../../../";
     return File.ReadAllText(Path.Join(basePath, "dtmi/samples/aninterface-1.json"));
@@ -10,7 +10,15 @@ string readFileFromDtmi()
 
 
 var parser = new ModelParser();
-var parserResult = await parser.ParseAsync(new string[] { readFileFromDtmi() });
+
+var parserResult = await new ModelParser().ParseAsync(new string[] { ReadFile() });
+foreach (var item in parserResult.Values.Where(i => i.EntityKind == DTEntityKind.Telemetry))
+{
+    var telemetry = (DTTelemetryInfo)item;
+    Console.WriteLine($" [T]: {telemetry.Name} {telemetry.Schema.Id}");
+}
+
+
 
 var root = parserResult.Where(i => i.Value.ChildOf == null).FirstOrDefault();
 Console.WriteLine(root.Key);
@@ -23,24 +31,37 @@ foreach (var item in ((DTInterfaceInfo)parserResult[root.Key]).Contents)
     var pi = (DTPropertyInfo)item.Value;
     Console.WriteLine(pi.Schema.Id);
 
+    Console.WriteLine();
+    foreach (var sp in pi.SupplementalProperties)
+    {
+        Console.WriteLine("sp");
+        Console.WriteLine(sp.Key);
+        Console.WriteLine(sp.Value.ToString());
+    }
+
+    
     foreach (var st in pi.SupplementalTypes)
     {
         Console.WriteLine("st");
         Console.WriteLine(st);
     }
 
+    Console.WriteLine();
+    var currentUnit = (DTUnitInfo)pi.SupplementalProperties["dtmi:dtdl:property:unit;2"];
+    Console.WriteLine(currentUnit.Id);
+
 }
 
 //var model = await new DtdlModelParser().ParseAsync("dtmi/samples/anextendedinterface-1.json");
+var model = await new DtdlModelParser().ParseAsync(ReadFile());
+foreach (var t in model.Telemetries)
+{
+    Console.WriteLine($" [T] {t.Name} {t.DataType}");
+}
 
-//Console.WriteLine(model.Id);
+foreach (var p in model.Properties)
+{
+    Console.WriteLine($" [P] {p.Name} {p.DataType}");
+}
 
-//foreach  (var t in model.Telemetries)
-//{
-//    Console.WriteLine($" [T] {t.Name} {t.DataType}");
-//}
-
-//foreach (var p in model.Properties)
-//{
-//    Console.WriteLine($" [P] {p.Name} {p.DataType}");
-//}
+Console.WriteLine(model.Id);
