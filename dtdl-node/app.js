01a47b11c3cca5774e41ceea3f89c5c7254eadde
ridomin <ridomin@live.com>
2022-04-08 12:11:41 -0700

    fix js

--- dtdl-node/app.js
+++ dtdl-node/app.js
@@ -1,24 +1,39 @@
 import * as fs from 'fs'
 import { createParser }  from '@azure/dtdl-parser'
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 
-const basePath = './../'
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const basePath =  path.join(__dirname,'./../')
+
 const dtmiToPath = dtmi => `/${dtmi.toLowerCase().replace(/:/g, '/').replace(';', '-')}.json`
 
-const resolveDtmi = dtmi1 => {
-    console.log('resolving ' + dtmi1)
-    console.log(dtmiToPath(dtmi1))
-    // const resolvedDtdl =  fs.readFileSync(basePath + dtmiToPath(dtmi), 'utf-8')
-    // console.log(resolvedDtdl)
-   // return [resolvedDtdl]
+const resolveDtmi = dtmis => {
+    const result = []
+    dtmis.forEach(dtmi => {
+        const resolvedDtdl =  fs.readFileSync(path.join(basePath,dtmiToPath(dtmi)), 'utf-8')
+        result.push(JSON.parse(resolvedDtdl))
+    })
+    return result
 }
 
 async function main() {
-    const dtdlRaw = fs.readFileSync(basePath + 'dtmi/samplesv3/anextendedinterface-1.json', 'utf-8')
+    const dtdlRaw = fs.readFileSync(basePath + 'dtmi/samplesv2/anextendedinterface-1.json', 'utf-8')
     const modelParser = createParser()
-    modelParser.getModels = resolveDtmi;
+    modelParser.getModels = resolveDtmi.bind(resolveDtmi);
     const modelDict = await modelParser.parse([dtdlRaw])
-    Object.entries(modelDict).forEach(([dtmi, dtinfo]) => {
-        console.log(dtmi, dtinfo.entityKind)
+    Object.entries(modelDict).forEach(([dtmi, entity]) => {
+        //console.log(dtmi, dtinfo.entityKind)
+        switch (entity.entityKind) {
+            case 'property': {
+                console.log(` [P] ${entity.name} ${entity.schema?.id} ${!!entity.writable}`)
+                break;
+            }
+            case 'telemetry': {
+                console.log(` [T] ${entity.name} ${entity.schema?.id}`)
+                break;
+            }
+        }
     })
 }
 
