d24d1f31cfc33c26252128e5b3fde102d18f2edf
rido-min <rido-min@users.noreply.github.com>
2023-01-17 18:12:47 -0800

    paad to v3

--- dtdl-dotnet/Program.cs
+++ dtdl-dotnet/Program.cs
@@ -19,7 +19,27 @@ Console.WriteLine($"MaxDtdlVersion: {parser.MaxDtdlVersion}");
 string fileName = "dtmi/azureiot/paad/iotphone-1.json";
 Console.WriteLine(fileName);
 
-var parserResult = await parser.ParseModelAsync(readFile(fileName));
+ModelParserExtensions.InterfaceInfo parserResult = null;
+
+try
+{
+    parserResult = await parser.ParseModelAsync(readFile(fileName));
+}
+catch (DTDLParser.ParsingException pex)
+{
+    Console.WriteLine();
+    Console.Error.WriteLine(pex.Message);
+    Console.WriteLine();
+    foreach (var err in pex.Errors)
+    {
+        Console.WriteLine($"{err.ValidationID}");
+        Console.WriteLine(err.Cause);
+        Console.WriteLine(err.Action);
+        Console.WriteLine(  );
+    }
+    Environment.Exit(1);
+}
+
 
 foreach (var item in parserResult.Telemetries)
 {
@@ -51,26 +71,39 @@ foreach (var c in parserResult.Commands)
         Console.Write($" req: {ModelParser.GetTermOrUri(req.Schema.Id)} ");
         req.SupplementalTypes.ToList().ForEach(t => Console.Write(" " + ModelParser.GetTermOrUri(t)));
         req.SupplementalProperties.ToList().ForEach(p => Console.Write(" " + ((DTEnumValueInfo)p.Value).Name));
-    }    
+    }
+    Console.WriteLine();
 }
+
+var batCo = parserResult.ObjectModel.First(x => x.Key == new Dtmi(new Uri("dtmi:azureiot:PaaD:Sensors:Battery;1")));
+
+
 
 foreach (var compo in parserResult.Components)
 {
-    Console.WriteLine($"[Co] {compo.Name} {compo.Schema.Id}");
-    var compTels = parserResult.ObjectModel
-                    .Where(c => c.Value.EntityKind == DTEntityKind.Telemetry && c.Value.ChildOf == compo.Schema.Id)
-                    .Select(t => (DTTelemetryInfo)t.Value);
-    foreach (var t in compTels)
+    Console.WriteLine($"[Co] {compo.Name} {compo.Id}");
+
+    DTInterfaceInfo compoDef = (DTInterfaceInfo)parserResult.ObjectModel.First(o => o.Key == compo.Schema.Id).Value;
+    var compoTels = compoDef.Contents.Where(c => c.Value.EntityKind == DTEntityKind.Telemetry).Select(t => (DTTelemetryInfo)t.Value);
+    
+    foreach (var t in compoTels)
     {
-        Console.WriteLine($"    [CoT] {t.Name} {t.Schema.Id}");
+        Console.Write($"    [CoT] {t.Name} {ModelParser.GetTermOrUri(t.Schema.Id)}");
+        t.SupplementalProperties.ToList().ForEach(p => Console.Write(" " + ((DTEnumValueInfo)p.Value).Name));
+        t.SupplementalTypes.ToList().ForEach(t => Console.Write("   " + ModelParser.GetTermOrUri(t)));
+        Console.WriteLine();
     }
 
-    var compProps = parserResult.ObjectModel
-                    .Where(c => c.Value.EntityKind == DTEntityKind.Property && c.Value.ChildOf == compo.Schema.Id)
+    var compProps = compoDef.Contents
+                    .Where(c => c.Value.EntityKind == DTEntityKind.Property)
                     .Select(t => (DTPropertyInfo)t.Value);
 
     foreach(var p in compProps)
     {
-        Console.WriteLine($"    [CoP] {p.Name} {p.Schema.Id}");
+        Console.Write($"    [CoP] {p.Name} {ModelParser.GetTermOrUri(p.Schema.Id)}");
+        p.SupplementalProperties.ToList().ForEach(p => Console.Write("    " + ((DTEnumValueInfo)p.Value).Name));
+        p.SupplementalTypes.ToList().ForEach(t => Console.Write("     " + ModelParser.GetTermOrUri(t)));
+        Console.WriteLine();
+        p.UndefinedTypes.ToList().ForEach(u => Console.Write(u));
     }
 }
